696bf92e33a6f103234e6c79dc85af975400504f
cxs92929 <zcx92929@outlook.com>
2019-04-22 20:07:27 +0800

    脚本自动签名

--- Assets/Editor/JenkinsAdapter.cs
+++ Assets/Editor/JenkinsAdapter.cs
@@ -3,9 +3,20 @@ using UnityEditor;
 
 public  class JenkinsAdapter
 {
-   // [MenuItem("Jenkins/JenkinsBuild")] 
+   [MenuItem("Jenkins/JenkinsBuild")] 
     public static void Build() {
         EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.Android);
+
+         // keystore 路径, G:\keystore\one.keystore
+        PlayerSettings.Android.keystoreName = "/Users/jiyuwangluo/Ceshi/userzs.keystore";
+        // one.keystore 密码
+        PlayerSettings.Android.keystorePass = "maczuihao8";
+
+        // one.keystore 别名
+        PlayerSettings.Android.keyaliasName = "userzs";
+        // 别名密码
+        PlayerSettings.Android.keyaliasPass = "maczuihao8";
+
         List<string> sceneList = new List<string>();
         EditorBuildSettingsScene[] temp = EditorBuildSettings.scenes;
         for (int i = 0, iMax = temp.Length; i < iMax; ++i) sceneList.Add(temp[i].path);
