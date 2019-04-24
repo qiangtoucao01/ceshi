c56e7aca7ad36d2612bf7f7be05acbf8e6b46b5a
cxs92929 <zcx92929@outlook.com>
2019-04-24 15:10:37 +0800

    开始使用ipa打包

--- Assets/Editor/JenkinsAdapter.cs
+++ Assets/Editor/JenkinsAdapter.cs
@@ -5,22 +5,22 @@ public  class JenkinsAdapter
 {
    [MenuItem("Jenkins/JenkinsBuild")] 
     public static void Build() {
-        EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.Android);
+        EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.iOS);
 
          // keystore 路径, G:\keystore\one.keystore
-        PlayerSettings.Android.keystoreName = "/Users/jiyuwangluo/Ceshi/userzs.keystore";
+     /*   PlayerSettings.Android.keystoreName = "/Users/jiyuwangluo/Ceshi/userzs.keystore";
         // one.keystore 密码
         PlayerSettings.Android.keystorePass = "maczuihao8";
 
         // one.keystore 别名
         PlayerSettings.Android.keyaliasName = "userzs";
         // 别名密码
-        PlayerSettings.Android.keyaliasPass = "maczuihao8";
+        PlayerSettings.Android.keyaliasPass = "maczuihao8";*/
 
         List<string> sceneList = new List<string>();
         EditorBuildSettingsScene[] temp = EditorBuildSettings.scenes;
         for (int i = 0, iMax = temp.Length; i < iMax; ++i) sceneList.Add(temp[i].path);
-        BuildPipeline.BuildPlayer(sceneList.ToArray(), "./test.apk", BuildTarget.Android, BuildOptions.None);
+        BuildPipeline.BuildPlayer(sceneList.ToArray(), "testipa", BuildTarget.iOS, BuildOptions.None);
         
     }
 }
